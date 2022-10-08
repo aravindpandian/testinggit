4273549ceffd00f86d2222fb6e2191d8eb03e605
Aravind PandianRamasamy <aravind.p16@ad.infosys.com>
2022-10-09 02:32:56 +0530

    Adding the updated code to main branch

--- first.php
+++ first.php
@@ -1,5 +1,5 @@
 <?php
-echo "Adding 1st line";
+echo "Adding 1st line in main branch only";
 echo "Adding the second line";
 echo "this should be the line";
 echo "Adding merge conflict in 1st line";
